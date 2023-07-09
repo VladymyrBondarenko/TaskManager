46c4985561f0cc3f2994e00dc36af193ebfb38a9
Бондаренко Владимир Валерьевич <vladymyr.bondarenko1@gmail.com>
2023-07-09 13:40:51 +0300

    Added tests project

---
+++ TaskManager.Infrastructure.Tests/TaskServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Infrastructure.Services;
+
+namespace TaskManager.Infrastructure.Tests
+{
+    public class TaskServiceTests
+    {
+        private readonly TaskService _taskService;
+
+        public TaskServiceTests()
+        {
+            _taskService = new TaskService();
+        }
+
+        [Fact]
+        public async Task GetTasks_ReturnsListOfTasks_WithNoEmptyNames()
+        {
+            var tasks = await _taskService.GetTasks();
+            Assert.True(tasks.All(x => !string.IsNullOrWhiteSpace(x.Name)));
+        }
+    }
+}
